3b28ffbf1d604d39780f4141b9e173eea8786297
Shunsuke Hioki <shunsuke_hioki@fringe81.com>
2021-02-11 19:49:14 +0900

    useStateの巻

---
+++ react_hooks_test/src/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const stories =[
+    {name:"ほげほげ"},
+    {name:"FugaFuga"},
+    {name:"Fuga"}
+  ]
+  const [searchTerm,setInput] = React.useState('')
+
+  const handleSearch = event =>{
+    setInput(event.target.value);
+  }
+
+
+  const searchedStories = stories.filter(function(story){
+    return story.name.toLowerCase().includes(searchTerm.toLowerCase())
+  })
+  return (
+    <div>
+      <Search onSearch={handleSearch}/>
+      <hr/>
+      <List list={searchedStories}/>
+    </div>
+  );
+}
+
+export default App;
+
+const List = props =>{
+  return(
+  props.list.map(item=> {
+    return (
+      <div>{item.name}</div>
+    )
+  })
+  )
+}
+
+
+const Search =props =>{
+  return (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" onChange={props.onSearch}/>
+  </div>
+  )
+};
