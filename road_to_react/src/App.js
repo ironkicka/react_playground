2f25fe264d880ba4a493e492c9473dcda7302d40
Shunsuke Hioki <>
2021-04-03 16:15:07 +0900

    added SearchForm component

--- road_to_react/src/App.js
+++ road_to_react/src/App.js
@@ -108,29 +108,20 @@ const App = () => {
     setSearchTerm(event.target.value);
   }
 
-  const handleSearchSubmit = () =>{
+  const handleSearchSubmit = (event) =>{
    setUrl(`${API_ENDPOINT}${searchTerm}`)
+    event.preventDefault();//HTMLフォームのデフォルトの挙動(リロード)を防ぐ
   }
 
   return (
     <div>
       <h1>My Hacker Stories</h1>
 
-      <InputWithLabel
-        id="search"
-        value={searchTerm}
-        onInputChange={handleSearchInput}
-
-      >
-        <strong>Search:</strong>
-      </InputWithLabel>
-      <button
-        type="button"
-        disabled={!searchTerm}
-        onClick={handleSearchSubmit}
-      >
-        Submit
-      </button>
+      <SearchForm
+        searchTerm={searchTerm}
+        onSearchInput={handleSearchInput}
+        onSearchSubmit={handleSearchSubmit}
+      />
 
       <hr />
       {stories.isNoResult && <p>No Results ... </p>}
@@ -144,6 +135,28 @@ const App = () => {
   );
 };
 
+
+const SearchForm = (
+  searchTerm,
+  onSearchInput,
+  onSearchSubmit
+)=>(
+  <form onSubmit={onSearchSubmit}>
+    <InputWithLabel
+      id="search"
+      value={searchTerm}
+      onInputChange={onSearchInput}
+    >
+      <strong>Search:</strong>
+    </InputWithLabel>
+    <button
+      type="button"
+      disabled={!searchTerm}
+    >
+      Submit
+    </button>
+  </form>
+)
 const InputWithLabel = ({
   id,
   value,
